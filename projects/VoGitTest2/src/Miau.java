90f5dde171e126109e827516a9b4288e08bc5913
maxleodax <max.leohub@hotmail.com>
2018-11-26 15:24:29 +0100

    commit #004

--- projects/VoGitTest2/src/Miau.java
+++ projects/VoGitTest2/src/Miau.java
@@ -3,5 +3,7 @@ public class Miau {
   public static void main(String[] args) {
     System.out.println("Miau");
     System.out.println("Miau Möög");
+    System.out.println("Miau Möög Möög");
+    
   }
 }
