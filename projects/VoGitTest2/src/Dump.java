46938fc9273f74f38d5783d6512d35caeea20a63
maxleodax <max.leohub@hotmail.com>
2018-11-27 00:59:01 +0100

    Commit B_01_12

--- projects/VoGitTest2/src/Dump.java
+++ projects/VoGitTest2/src/Dump.java
@@ -4,6 +4,13 @@ public class Dump {
     System.out.println("Hello Dump");
     System.out.println("Hello Dump");
     System.out.println("Hello Dump from Branch_01");
+    System.out.println("Hello Dump again from Branch_01");
+    System.out.println("Hello Dump again from Branch_01 Stashy Bashy");
+    System.out.println("Hello Dump again from Branch_01 Stashy Bashy");
+    System.out.println("Hello Dump again from Branch_01 Stashy Bashy");
+    System.out.println("Hello Dump again from Branch_02 Stashy Bashy");
+    System.out.println("Hello Dump again from Branch_03 Stashy Bashy");
+    System.out.println("Hello Dump again from Branch_04 Stashy Bashy");
   }
 
 }
