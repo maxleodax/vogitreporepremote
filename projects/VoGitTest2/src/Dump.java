57e68014b360d5e5100eed68464f7534727e7317
maxleodax <max.leohub@hotmail.com>
2018-11-27 00:23:51 +0100

    commit #008

--- projects/VoGitTest2/src/Dump.java
+++ projects/VoGitTest2/src/Dump.java
@@ -2,6 +2,7 @@ public class Dump {
   public static void main(String[] args) {
     System.out.println("Hello Dump");
     System.out.println("Hello Dump");
+    System.out.println("Hello Dump");
   }
 
 }
